dc73c88a76e6a5594f7bddc2e1cdb5ae3499cd6b
Andrea Kereliuk <andkerel@gmail.com>
2019-04-19 00:25:25 +0100

    Try basic fetch() of meter data

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,15 @@
 'use strict';
 
+//URL
+var url = 'https://widgister.herokuapp.com/challenge/frontend';
+
+//DOM Nodes
 var value = document.getElementById('m-value'),
     min = document.getElementById('m-min'),
     max = document.getElementById('m-max'),
     format = document.getElementById('m-format'),
     unit = document.getElementById('m-unit');
 
+fetch(url).then(response => response.json()).then((data) => {
+  console.log(data);
+})
